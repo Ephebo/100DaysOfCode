923018311ed94aac1cc11a005c4c79c21987393b
Matheus <118321568+Ephebo@users.noreply.github.com>
2023-02-27 16:33:14 -0300

    Update fizzbuzz.py

--- 100DaysOfCode/fizzbuzz.py
+++ 100DaysOfCode/fizzbuzz.py
@@ -11,15 +11,20 @@ print("""
   
   
   
-  
+# entrada do usuário
 n = int(input("Digite um número: "))  
-    
+
+# contagem de números    
 count = 0 
 for each_number in range(1,n+1):
     count = each_number
+
+# checar condições
     if count % 5 == 0 and count % 3 == 0:
       print("FizzBuzz")    
-        
+      # se o número for divisível por 5 e 3, print'fizzBuzz'.
+      # objetivo é iterar todos os múltiplos de 5 e 3      
+      
     elif count % 5 == 0:
       print("Buzz")
         
