4a32580c7c3eb40e95f54e0ffd2e7c7eb228d5e3
Matheus <118321568+Ephebo@users.noreply.github.com>
2023-02-27 16:55:21 -0300

    Update fizzbuzz.py

--- 100DaysOfCode/fizzbuzz.py
+++ 100DaysOfCode/fizzbuzz.py
@@ -10,7 +10,7 @@ print("""
 """)
   
   
-  
+
 # entrada do usuário
 n = int(input("Digite um número: "))  
 
@@ -20,17 +20,23 @@ for each_number in range(1,n+1):
     count = each_number
 
 # checar condições
+# # se o número for divível por 5 e 3, serão múltiplos dos mesmos.
     if count % 5 == 0 and count % 3 == 0:
-      print("FizzBuzz")    
-      # se o número for divisível por 5 e 3, print'fizzBuzz'.
-      # objetivo é iterar todos os múltiplos de 5 e 3, ou seja, todos os números menos 5 e 3      
+      print("FizzBuzz")     
+    # checar se a variável "count" é divisível por 5 e 3(true): printar'FizzBuzz'
       
     elif count % 5 == 0:
+    #checar se a variável é divisível por 5
+    # se for(true), print(buzz) 
       print("Buzz")
         
     elif count % 3 == 0:
       print("Fizz")
+    # checar se a variável é divísivel por 3
+    # se for(true): print("Fizz")
+    
     else:
       print(count)    
-
+    # caso tudo não for satisfeito, print o número na variável.
+    
 input("Pressione Enter para sair...")
